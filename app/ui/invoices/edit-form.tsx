241d13bb87b4b005e7b24b4a87843fba68332d7a
Ernest Ho <ernest.heh@gmail.com>
2024-01-11 18:09:52 +0800

    add lint script

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -121,7 +121,7 @@ export default function EditInvoiceForm({
         >
           Cancel
         </Link>
-        <Button type="submit">Edit Invoice</Button>
+        <Button type="submit">Save Edit</Button>
       </div>
     </form>
   );
