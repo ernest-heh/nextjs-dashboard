b5a3244d41ee11c4927c608cc016c05c6fcb9f14
Ernest Ho <ernest.heh@gmail.com>
2024-01-12 14:49:29 +0800

    remove unused import from login form

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -10,7 +10,6 @@ import { ArrowRightIcon } from '@heroicons/react/20/solid';
 import { Button } from './button';
 import { useFormState, useFormStatus } from 'react-dom';
 import { authenticate } from '../lib/actions';
-import { error } from 'console';
 
 export default function LoginForm() {
   const [errorMessage, dispatch] = useFormState(authenticate, undefined);
